b83e2ae8effe9f4ae0d576fd590ec6981774eca2
RBhola <36428741+RBhola@users.noreply.github.com>
2018-02-15 11:10:31 -0500

    Add files via upload

---
+++ 05/CommentsAndSlashes.java
@@ -0,0 +1,14 @@
+public class CommentsAndSlashes {
+	public static void main(String[] args)
+	{
+		//A random comment, it cannot be seen
+		//Things after the//is ignored.
+
+		System.out.println("I could have the code like this."); // and the comment after is not paid attention to
+
+		//You can comment to "disable" or comment out of a piece of code:
+  		//System.out.println("This won't run.")
+
+		System.out.println("This will run.");
+	}
+}
