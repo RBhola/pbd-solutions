58fa221b9db870b92604d9482c0b599c58d353aa
RBhola <36428741+RBhola@users.noreply.github.com>
2018-02-27 11:02:37 -0500

    Add files via upload

--- 21/WhatIf.java
+++ 21/WhatIf.java
@@ -4,10 +4,10 @@ public class WhatIf {
         int cats = 30;
         int dogs = 15;
         
-        if (people < cats) {
+        if (people > cats) {
         System.out.println("Too many cats! The world is doomed!");
         }
-        
+        //the statement above was made to not be displayed
         if (people > cats) {
         System.out.println("Not many cats! The world is saved!");
         }
@@ -19,13 +19,13 @@ public class WhatIf {
         if (people > dogs) {
         System.out.println("The world is dry!");
         }
-        
+        //the "if" statement makes the staements under display if it is true
         dogs += 5;
         
         if (people >= dogs) {
         System.out.println("People are greater than or equal to dogs.");
         }
-        
+        //the brackets determine the boundaries for which statements should be considered true or false
         if (people <= dogs) {
         System.out.println("People are less than or equal to dogs.");
         }
