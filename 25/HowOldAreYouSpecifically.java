5f1d53d93e48329cfb8a4742f5ff205e259cf186
RBhola <36428741+RBhola@users.noreply.github.com>
2018-03-02 11:08:21 -0500

    Add files via upload

---
+++ 25/HowOldAreYouSpecifically.java
@@ -0,0 +1,17 @@
+import java.util.Scanner:
+
+public class HowOldAreYouSpecifically {
+    public static void main(String[] args) {
+        Scanner input = ne
+        Scanner(System.in);
+        
+        System.out.print("Hey, what's your name? ");
+        String name = input.next();
+        
+        System.out.println("Ok, " + name + ", how old are you?");
+        int age = input.nextInt();
+        System.out.println();
+        
+        if (age < 16) {
+            System.out.println("You can't drive.");
+        } else if (age )
